a80fc79dad0a7b81263ee63c2884d94e26d7dc13
Tumelo ngoma <72390075+Grab20@users.noreply.github.com>
2026-07-09 22:27:40 +0200

    Integrate push notifications in AppLayout

--- mobile/app/(app)/_layout.tsx
+++ mobile/app/(app)/_layout.tsx
@@ -1,10 +1,12 @@
 import { Redirect, Stack } from 'expo-router'
 import { useAuthStore } from '../../src/stores/authStore'
 import { LoadingScreen } from '../../src/components/LoadingScreen'
+import { usePushNotifications } from '../../src/hooks/usePushNotifications'
 
 export default function AppLayout() {
   const session = useAuthStore((s) => s.session)
   const isInitializing = useAuthStore((s) => s.isInitializing)
+  usePushNotifications()
 
   if (isInitializing) return <LoadingScreen />
   if (!session) return <Redirect href="/login" />
